46e62d9e2e376d1a6d545eb6af6e2253c9a25669
trba1810 <trba8l@live.com>
2023-01-19 02:08:25 +0100

    added icons

--- src/Compontents/ContactPages/Contact.jsx
+++ src/Compontents/ContactPages/Contact.jsx
@@ -2,10 +2,43 @@ import React from "react";
 
 const Contact = (props) => {
   return (
-    <div>
-      <button className="btn btn-danger form-control">
-        {props.contact.name}
-      </button>
+    <div
+      className="row p-md-2 mb-2"
+      style={{ borderRadius: "20px", border: "1px solid #555" }}
+    >
+      <div className="col-2 col-md-1 pt-2 pt-md-1">
+        <img
+          src={`https://ui-avatars.com/api/?name=${props.contact.name}`}
+          style={{ width: "80%" }}
+          alt="avatar"
+        />
+      </div>
+      <div className="col-6 col-md-5 text-warning pt-0">
+        <span className="h4">{props.contact.name}</span>
+        <br />
+        <div className="text-white-50">
+          {props.contact.email}
+          <br />
+          {props.contact.phone}
+        </div>
+      </div>
+      <div className="col-2 col-md-2 pt-md-3">
+        <button
+          className={`btn btn-sm sm-1 ${
+            props.contact.isFavorite ? "btn-warning" : "btn-outline-warning"
+          }`}
+        >
+          <i class="bi bi-star" style={{ size: "1rem" }}></i>
+        </button>
+      </div>
+      <div className="col-2 col-md-3 pt-md-3">
+        <button className="btn btn-primary btn-sm m-1">
+          <i class="bi bi-pencil-square" style={{ size: "1rem" }}></i>
+        </button>
+        <button className="btn btn-danger btn-sm m-1">
+          <i class="bi bi-trash-fill" style={{ size: "1rem" }}></i>
+        </button>
+      </div>
     </div>
   );
 };
