48d52ffdd56b7b9901eed9b860c85e556769bb14
trba1810 <trba8l@live.com>
2023-01-20 09:39:39 +0100

    added axios

--- src/Compontents/ContactPages/AddRandomContact.jsx
+++ src/Compontents/ContactPages/AddRandomContact.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { getRandomUser } from "../../Utility/api";
 
-const AddRandomContact = () => {
+const GetRandomContact = async (props) => {
+  const responseAPI = await getRandomUser();
+  console.log(responseAPI);
+
+  return props.handleAddRandomContact({
+    name: responseAPI.data.first_name + " " + responseAPI.data.last_name,
+    email: responseAPI.data.email,
+    phone: responseAPI.data.phone_number,
+  });
+};
+
+const AddRandomContact = (props) => {
   return (
     <div>
-      <button className="btn btn-danger form-control">
+      <button
+        className="btn btn-success form-control"
+        onClick={() => GetRandomContact(props)}
+      >
         Dodaj random kontakt
       </button>
     </div>
--- src/Compontents/ContactPages/ContactIndex.jsx
+++ src/Compontents/ContactPages/ContactIndex.jsx
@@ -92,16 +92,31 @@ export default class ContactIndex extends Component {
     });
   };
 
+  handleAddRandomContact = (newContact) => {
+    const newFinalContact = {
+      ...newContact,
+      id: this.state.contactList[this.state.contactList.length - 1].id + 1,
+      isFavorite: false,
+    };
+    this.setState((prevState) => {
+      return {
+        contactList: prevState.contactList.concat([newFinalContact]),
+      };
+    });
+  };
+
   render() {
     return (
       <div>
         <Header />
         <div className="container" style={{ minHeight: "85lv" }}>
           <div className="row py-3">
-            <div className="col-4 offset-2">
-              <AddRandomContact />
+            <div className="col-4 offset-2 row">
+              <AddRandomContact
+                handleAddRandomContact={this.handleAddRandomContact}
+              />
             </div>
-            <div className="col-4">
+            <div className="col-4 row">
               <RemoveAllContact />
             </div>
             <div className="row py-2">
